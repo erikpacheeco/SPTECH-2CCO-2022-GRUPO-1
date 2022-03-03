b33bf51a4c3d45ae6674193f9de8a5febd439e8f
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-03-03 16:13:55 -0300

    create spring structure

---
+++ estrutura-de-dados/src/test/java/com/sptech/estruturadedados/EstruturaDeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptech.estruturadedados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstruturaDeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
