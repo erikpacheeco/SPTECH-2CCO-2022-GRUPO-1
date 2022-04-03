e9739f72107a624792faa02b2a4737892d50b181
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-04-02 23:21:27 -0300

    unificação de código da solicitação de resgate

--- site/js/script.js
+++ site/js/script.js
@@ -1,10 +1,40 @@
-let idSolicitar = document.querySelector("#idSolicitar");
-let idTextSolicitar = document.querySelector("#textSolicitar");
+// solicitar resgate
+let containerResgate = document.createElement("div");
+let spanTextSolicitar = document.createElement("span");
+let iconResgate = document.createElement("img");
 
-idSolicitar.addEventListener("mouseover", (evt) => {
-  idTextSolicitar.style.display = "block";
+// span
+spanTextSolicitar.id = "textSolicitar";
+spanTextSolicitar.classList.add("text-resgate");
+spanTextSolicitar.innerHTML = "Solicitar Resgate";
+
+// img
+iconResgate.classList.add("icon-resgate");
+iconResgate.src = "./svg/icon_resgate.svg";
+iconResgate.alt = "Telefone com pata de cachorro como símbolo";
+
+// container
+containerResgate.id = "idSolicitar";
+containerResgate.classList.add("container-resgate");
+
+// append
+containerResgate.appendChild(spanTextSolicitar);
+containerResgate.appendChild(iconResgate);
+
+document.querySelector("body").appendChild(containerResgate);
+
+// events
+
+containerResgate.addEventListener("mouseover", (evt) => {
+  spanTextSolicitar.style.display = "block";
 });
 
-idSolicitar.addEventListener("mouseout", (evt) => {
-    idTextSolicitar.style.display = "none";
+containerResgate.addEventListener("mouseout", (evt) => {
+    spanTextSolicitar.style.display = "none";
 });
+
+containerResgate.addEventListener("click", (evt) => {
+  window.location.href = "#";
+})
+
+// fim solicitar resgate
