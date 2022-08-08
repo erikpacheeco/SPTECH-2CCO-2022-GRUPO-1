5ced98da143462c7e2e4a90cc9a8e909684d60e5
Lucas Mesquita de Souza <lucas.msouza@bandtec.com.br>
2022-08-08 17:40:02 -0300

    separando lista e cadastro de pet

--- petfinder-app/src/Pages/CadastroPet/index.jsx
+++ petfinder-app/src/Pages/CadastroPet/index.jsx
@@ -1,37 +1,8 @@
-import './styles.css';
-import HeaderUser from "../../Components/HeaderUser";
-import NavItem from "../../Components/NavItem";
-import PetListItem from "../../Components/PetListItem";
+import "./styles.css";
 
 function CadastroPet() {
-
-    function handleAddItemList() {
-        console.log("click!");
-    }
-
     return(
-        <>
-            <HeaderUser itens={[
-                <NavItem label="Dashboard" />,
-                <NavItem label="Padrinhos" />,
-                <NavItem label="Demandas" />,
-                <NavItem isSelected={true} label="Pets" />
-            ]}
-            />
-
-            <div className="cadastro-pet-container">
-                <div className="cadastro-pet-title">
-                    <h2>Pets Cadastrados</h2>
-                    <div className="cadastro-pet-add-icon" onClick={handleAddItemList}>+</div>
-                </div>
-                <div id="idCadstroPetList" className="cadastro-pet-list">
-                    <PetListItem nome="Rubens" especie="Cachorro" raca="SRD" idade="5" peso="34"/>
-                    <PetListItem nome="Rubens" especie="Cachorro" raca="SRD" idade="5" peso="34"/>
-                    <PetListItem nome="Rubens" especie="Cachorro" raca="SRD" idade="5" peso="34"/>
-                </div>
-            </div>
-                
-        </>
+        <h1>Helo CadastroPet</h1>
     )
 }
 
--- petfinder-app/src/routes.js
+++ petfinder-app/src/routes.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Cadastro from "./Pages/Cadastro";
 import HomeUser from "./Pages/HomeUser";
-import CadastroPet from "./Pages/CadastroPet";
+import ListaPet from "./Pages/ListaPet";
+import CadastroPet from "./Pages/CadastroPet"
 
 function Rotas() {
     return (
@@ -11,6 +12,7 @@ function Rotas() {
                 <Route path="/cadastro" element={<Cadastro/>} />
                 <Route path="/" element={<Login/>}/>
                 <Route path="/home-user" element={<HomeUser/>} />
+                <Route path="/lista-pet" element={<ListaPet/>} />
                 <Route path="/cadastro-pet" element={<CadastroPet/>} />
             </Routes>
         </BrowserRouter>
