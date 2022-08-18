bab0176955fd9c2dcf57bd512da4c05d9e43fc27
Lucas Mesquita de Souza <lucas.msouza@bandtec.com.br>
2022-08-18 19:55:17 -0300

    api-msg

---
+++ petfinder-app/src/Components/FormContainer/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function FormContainer({title}) {
+    return(
+        <div className="cad-user-container">
+            <h1>Form</h1>
+        </div>
+    );
+}
+
+export default FormContainer;
--- petfinder-app/src/Pages/CadastroPet/index.jsx
+++ petfinder-app/src/Pages/CadastroPet/index.jsx
@@ -1,8 +1,20 @@
 import "./styles.css";
+import HeaderUser from "../../Components/HeaderUser";
+import NavItem from "../../Components/NavItem";
+import FormContainer from "../../Components/FormContainer";
 
 function CadastroPet() {
     return(
-        <h1>Helo CadastroPet</h1>
+        <>
+            <HeaderUser itens={[
+                <NavItem label="Dashboard" />,
+                <NavItem label="Padrinhos" />,
+                <NavItem label="Demandas" />,
+                <NavItem isSelected={true} label="Pets" />
+            ]} />
+
+            <FormContainer title="Cadastro de Pet"/>
+        </>
     )
 }
 
