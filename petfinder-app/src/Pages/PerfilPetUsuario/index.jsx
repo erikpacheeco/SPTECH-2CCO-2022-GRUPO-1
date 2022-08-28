1b0c4f48ba96c596ede69997edf029d138b3087c
RafaAmancioSoares <rafaela.soares@bandtec.com.br>
2022-08-28 17:06:20 -0300

    Conectando FE com a API na página de Perfil do Pet do Usuário

--- petfinder-app/src/Pages/PerfilPetUsuario/index.jsx
+++ petfinder-app/src/Pages/PerfilPetUsuario/index.jsx
@@ -6,19 +6,16 @@ import api from "../../Api"
 
 function PerfilPetUsuario() {
 
-    const [valuesInteresse, setValuesInteresse] = useState([])
+    const [infoPet, setInfoPet] = useState([])
     const [preferencias, setPreferencias] = useState([])
 
     useEffect(() => {
-        api.get("/pets/caracteristicas").then((res) => {
-            try {
-                console.log(res.data)
-                setPreferencias(res.data)
-            } catch (error) {
-                console.log(error)
-            }
+        api.get("/pets/1/perfil").then((res) => {
+            setInfoPet(res.data)
+            setPreferencias(res.data.caracteristicas)
         })
-    }, [])
+
+    })
 
     return(
         <>
@@ -32,7 +29,7 @@ function PerfilPetUsuario() {
                 <div className="perfil-pet-usuario-container">
 
                     <div className="perfil-pet-usuario-foto">
-                        <img src="https://img.freepik.com/fotos-gratis/o-gato-vermelho-ou-branco-eu-no-estudio-branco_155003-13189.jpg?w=2000" alt="" />
+                        <img src={infoPet.caminhoImagem} alt="" />
                     </div>
 
                     <div className="perfil-pet-usuario-container-info">
@@ -40,8 +37,12 @@ function PerfilPetUsuario() {
                         <div className="perfil-pet-usuario-container-info-instituicao">
                             
                             <div className="perfil-pet-usuario-info-instituicao-text">
-                                <h1>Delivery de Gatinhos</h1>
-                                <h1>12km</h1>
+                                {
+                                    <>
+                                        <h1>{infoPet.instituicao}</h1>
+                                        <h1>{infoPet.distancia}</h1>
+                                    </>
+                                }
                             </div>
                         
                         </div>
@@ -53,39 +54,39 @@ function PerfilPetUsuario() {
                                 <div className="perfil-pet-usuario-info-pet">
                                     <div className="perfil-pet-usuario-info-pet-text">
                                         <div className="perfil-pet-usuario-info-nome">
-                                            <h2>Vanessa</h2>
+                                            <h2>{infoPet.nome}</h2>
                                         </div>
                                         
                                         <div className="perfil-pet-usuario-info-idade">
                                             <p>Idade: </p>
-                                            <span>111</span>
+                                            <span>{infoPet.idade}</span>
                                         </div>
 
                                         <div className="perfil-pet-usuario-info-especie">
                                             <p>Especie: </p>
-                                            <span>111</span>
+                                            <span>{infoPet.especie}</span>
                                         </div>
 
                                         <div className="perfil-pet-usuario-info-porte">
                                             <p>Porte: </p>
-                                            <span>111</span>
+                                            <span>{infoPet.porte}</span>
                                         </div>
 
                                         <div className="perfil-pet-usuario-info-raca">
                                             <p>Raça: </p>
-                                            <span>111</span>
+                                            <span>{infoPet.raca}</span>
                                         </div>
 
                                         <div className="perfil-pet-usuario-info-mimos">
                                             <p>Mimos por mês: </p>
-                                            <span>111</span>
+                                            <span>{infoPet.mimosPorMes}</span>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="perfil-pet-usuario-info-descricao">
                                     <p>Descrição: </p>
-                                    <textarea className="perfil-pet-usuario-descricao-textarea" id="" cols="30" rows="15"></textarea>
+                                    <textarea className="perfil-pet-usuario-descricao-textarea" id="" cols="30" rows="15" value={infoPet.descricao}></textarea>
                                     
                                 </div>
 
@@ -102,35 +103,12 @@ function PerfilPetUsuario() {
                                     <div className="perfil-pet-usuario-caracteristica-btn">
                                         {
                                             preferencias.map((pref) => (
-                                                <>
-                                                    <input
-                                                        className="cad-user-hide"
-                                                        value={pref.caracteristicas}
-                                                        type="checkbox"
-                                                        id={pref.id}
-                                                    />
+                                                <>   
                                                     <button
                                                         type="button"
                                                         className="btn-preferencia"
-                                                        id={pref.id + "-btn"}
-                                                        onClick={() => {
-                                                            let input = document.getElementById(pref.id)
-                                                            let btn = document.getElementById(pref.id + "-btn")
-                                                            const { value } = input
-
-                                                            input.checked = !document.getElementById(pref.id).checked
-                                                            
-                                                            if (input.checked) {
-                                                                btn.classList.replace("btn-preferencia", "btn-preferencia-checked")
-                                                                setValuesInteresse([...valuesInteresse,{caracteristicas: value }])
-                                                            }
-                                                            else {
-                                                                btn.classList.replace("btn-preferencia-checked", "btn-preferencia")
-                                                                setValuesInteresse( valuesInteresse.filter((e) => e.caracteristicas !== value))
-                                                            }
-                                                        }}
                                                     >
-                                                        {pref.caracteristicas}
+                                                        {pref}
                                                     </button>
                                                 </>
                                             ))
@@ -141,7 +119,7 @@ function PerfilPetUsuario() {
                                 <div className="perfil-pet-usuario-info-adocao-acao-container">
                                     <div className="perfil-pet-usuario-info-adocao-acao">
                                         <p>Quem cuida de mim?</p>
-                                        <h2>PETY</h2>
+                                        <h2>{infoPet.instituicao}</h2>
                                     </div>
 
                                     <div className="perfil-pet-usuario-info-adocao-acao-btn">
