b93e0b8a6cf0743933a061b25e73b685ebfbec60
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-09-03 20:55:20 -0300

    testing remove http

--- petfinder-app/src/Api.js
+++ petfinder-app/src/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const dns = "ec2-52-54-137-10.compute-1.amazonaws.com"
 
 const api = axios.create({
-    baseURL:`http://${dns}:8080`
+    baseURL:`${dns}:8080`
 }) 
 
 export default api;
