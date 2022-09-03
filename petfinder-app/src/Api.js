3e5446c4e27ea0a0d7e45597231aa620f461ffe9
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-09-03 17:42:06 -0300

    production test

--- petfinder-app/src/Api.js
+++ petfinder-app/src/Api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const dns = "ec2-52-54-137-10.compute-1.amazonaws.com"
 
 const api = axios.create({
-    baseURL:"http://localhost:8080"
+    baseURL:`http://${dns}:8080`
 }) 
 
 export default api;
