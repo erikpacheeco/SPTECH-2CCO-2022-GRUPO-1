1c2cf07e043800ee3c1cd5103c51820027095fc6
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-09-04 23:49:22 -0300

    fix(post pet): endpoint agora exige uma estrutura mais simples

--- petfinder-api/src/main/java/petfinder/petfinderapi/service/ServicePet.java
+++ petfinder-api/src/main/java/petfinder/petfinderapi/service/ServicePet.java
@@ -1,17 +1,28 @@
 package petfinder.petfinderapi.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import petfinder.petfinderapi.entidades.Caracteristica;
+import petfinder.petfinderapi.entidades.Instituicao;
+import petfinder.petfinderapi.entidades.Pet;
+import petfinder.petfinderapi.entidades.PetHasCaracteristica;
 import petfinder.petfinderapi.entidades.Usuario;
+import petfinder.petfinderapi.repositorios.CaracteristicaRepositorio;
 import petfinder.petfinderapi.repositorios.InstituicaoRepositorio;
+import petfinder.petfinderapi.repositorios.PetHasCaracteristicaRepositorio;
 import petfinder.petfinderapi.repositorios.PetRepositorio;
 import petfinder.petfinderapi.repositorios.UsuarioRepositorio;
+import petfinder.petfinderapi.requisicao.PetRequest;
 import petfinder.petfinderapi.resposta.PetPerfil;
 import petfinder.petfinderapi.rest.DistanciaResposta;
 import petfinder.petfinderapi.service.exceptions.EntityNotFoundException;
+import petfinder.petfinderapi.service.exceptions.IdNotFoundException;
 import petfinder.petfinderapi.service.exceptions.NoContentException;
 
 @Service
@@ -25,6 +36,12 @@ public class ServicePet {
 
     @Autowired
     private InstituicaoRepositorio instituicaoRepository;
+
+    @Autowired
+    private CaracteristicaRepositorio caracteristicaRepositorio;
+    
+    @Autowired
+    private PetHasCaracteristicaRepositorio petHasCaracteristicaRepository;
 
     @Autowired
     private ServiceCep serviceCep;
@@ -97,4 +114,53 @@ public class ServicePet {
         // 404 not found
         throw new EntityNotFoundException(id);
     }
+
+    // returns created pet
+    public PetPerfil createPet(PetRequest novoPet) {
+        
+        Optional<Instituicao> optionalInstituicao = instituicaoRepository.findById(novoPet.getInstituicaoId());
+
+        if (optionalInstituicao.isPresent()) {
+            Pet pet = fromPetRequestToPet(novoPet, optionalInstituicao.get());
+            pet = petRepository.save(pet);
+
+            for (Integer caracteristicaId : novoPet.getCaracteristicas()) {
+
+                PetHasCaracteristica petHasCaracteristica = new PetHasCaracteristica();
+                Optional<Caracteristica> caracteristica = caracteristicaRepositorio.findById(caracteristicaId);
+                
+                if(caracteristica.isPresent()) {
+                    petHasCaracteristica.setCaracteristica(caracteristica.get());
+                    petHasCaracteristica.setPet(pet);
+                    petHasCaracteristica = petHasCaracteristicaRepository.save(petHasCaracteristica);
+                }
+            }
+
+            // 201 created
+            return new PetPerfil(pet);
+        }
+
+        // 404 instituicao not found
+        throw new IdNotFoundException(novoPet.getInstituicaoId(), "instituicaoId");
+    }
+
+    private Pet fromPetRequestToPet(PetRequest novoPet, Instituicao instituicao) {
+        Pet pet = new Pet();
+        pet.setAdotado(false);
+        pet.setCaminhoImagem("/");
+        pet.setDataNasc(novoPet.getDataNasc());
+        pet.setDescricao(novoPet.getDescricao());
+        pet.setDoente(novoPet.getDoente());
+        pet.setEspecie(novoPet.getEspecie());
+        pet.setNome(novoPet.getNome());
+        pet.setPorte(novoPet.getPorte());
+        pet.setPetHasCaracteristica(List.of());
+        pet.setSexo(novoPet.getSexo());
+        pet.setRaca(novoPet.getRaca());
+        pet.setInstituicao(instituicao);
+        
+        // falta lista de caracteristicas
+        return pet;
+    }
+
 }
