f1a7b41bfa1305b65bf156d6c8e12fe1a11f5884
Godoy <e.godoy@accenture.com>
2022-09-17 22:31:47 -0300

    Configurações do React App

--- petfinder-app/src/Api.js
+++ petfinder-app/src/Api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const dns = "ec2-52-54-137-10.compute-1.amazonaws.com"
+const dns = "localhost"
 
 const api = axios.create({
-    baseURL:`http://${dns}:8080`
+    baseURL:`https://${dns}:8080`
 }) 
 
 export default api;
