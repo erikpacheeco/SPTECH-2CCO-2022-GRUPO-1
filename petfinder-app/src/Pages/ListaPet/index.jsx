44304e5e6674e33ea1b95314f9aed32ebc69cc6e
erikpacheeco <erik.pacheco@bandtec.com.br>
2022-09-24 03:03:13 -0300

    alteração no código do header e sidebar e criação de funcões para automatizar suas criações de acordo com o nível de acesso

--- petfinder-app/src/Pages/ListaPet/index.jsx
+++ petfinder-app/src/Pages/ListaPet/index.jsx
@@ -1,22 +1,17 @@
 import './lista-pet.css';
 import HeaderApp from "../../Components/HeaderApp";
-import NavItem from "../../Components/NavItem";
 import PetListItem from "../../Components/PetListItem";
 import add from "../../Images/plus.svg"
 import file from "../../Images/file-txt.svg"
 import api from "../../Api"
 import React, { useEffect, useState } from 'react';
 import { calculateNewValue } from '@testing-library/user-event/dist/utils';
-import SideBarItem from "../../Components/SideBarItem";
-import Dashboard from "../../Images/data-graph.svg";
-import Pets from "../../Images/paw.svg";
-import Demandas from "../../Images/attention-icon.svg";
-import Padrinhos from "../../Images/padrinhos.svg";
-import MeuPerfil from "../../Images/people.svg";
-import PerfilInstituicao from "../../Images/user-business.svg";
-import Colaboradores from "../../Images/colaboradores.svg";
-import Duvida from "../../Images/duvida.svg";
+
 import VLibras from "@djpfs/react-vlibras"
+import headerFunctions from "../../functions/headerFunctions";
+import SideBarItem from "../../Components/SideBarItem";
+import NavItem from "../../Components/NavItem";
+
 
 function ListaPet() {
 
@@ -45,24 +40,8 @@ function ListaPet() {
 
             <div className="lista-pet-root">
                 <HeaderApp
-                    
-                    sideItens={[
-                        // <SideBarItem icon={Dashboard} label="Dashboard"/>,
-                        // <SideBarItem icon={Pets} label="Pets"/>,
-                        // <SideBarItem icon={Demandas} label="Demandas"/>,
-                        // <SideBarItem icon={Padrinhos} label="Padrinhos"/>,
-                        // <SideBarItem icon={MeuPerfil} label="Meu Perfil"/>,
-                        // <SideBarItem icon={PerfilInstituicao} label="Perfil Instituição"/>,
-                        // <SideBarItem icon={Colaboradores} label="Colaboradores Cadastrados"/>,
-                        // <SideBarItem icon={Duvida} label="Dúvida"/>
-                    ]}
-                
-                    itens={[
-                        <NavItem label="Dashboard" />,
-                        <NavItem label="Padrinhos" />,
-                        <NavItem label="Demandas" />,
-                        <NavItem isSelected={true} label="Colaboradores" />
-                    ]}
+                    sideItens={headerFunctions.sideBarNivelAcesso(objUser.nivelAcesso)}
+                    itens={headerFunctions.headerNivelAcesso(objUser.nivelnivelAcesso)}
                 />
 
                 <div className="lista-pet">
@@ -102,6 +81,9 @@ function ListaPet() {
             <VLibras forceOnload={true}></VLibras>
         </>
     )
+
+    
+
 }
 
 export default ListaPet;
