e280a397d022bbb4194721c493f8a903749c5336
RafaAmancioSoares <rafaela.soares@bandtec.com.br>
2022-09-24 02:02:30 -0300

    Adicionando função de edição de Pet

--- petfinder-app/src/Pages/PerfilPetInstituicao/index.jsx
+++ petfinder-app/src/Pages/PerfilPetInstituicao/index.jsx
@@ -5,20 +5,102 @@ import { useEffect, useState } from "react";
 import api from "../../Api"
 import React from "react";
 import { useParams } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2';
+import withReactContent from "sweetalert2-react-content";
 import VLibras from "@djpfs/react-vlibras"
+
+function resetValues() {
+    return { 
+        descricao: "",
+        especie: "",
+        idade: "",
+        instituicao: "",
+        isDoente: "",
+        mimosPorMes: "",
+        nome: "",
+        porte: "",
+        raca: ""
+    }
+}
 
 function PerfilPetInstituicao() {
 
     const [infoPet, setInfoPet] = useState([])
     const [preferencias, setPreferencias] = useState([])
+    const [values, setValues] = useState(resetValues)
+    
     const idPet = useParams()
+    const navigate = useNavigate()
+    const swal = withReactContent(Swal);
+
+    console.log(values)
+
     useEffect(() => {
-        api.get(`/pets/${idPet.id}/perfil`).then((res) => {
-            setInfoPet(res.data)
-            setPreferencias(res.data.caracteristicas)
-            console.log(infoPet)
-        })
+        if(values.descricao == "" && values.especie == "" && values.idade == "" && values.instituicao == "" &&
+        values.isDoente == "" && values.mimosPorMes == "" && values.nome == "" && values.porte == "" && values.raca == ""){
+            api.get(`/pets/${idPet.id}/perfil`).then((res) => {
+                setInfoPet(res.data)
+                setPreferencias(res.data.caracteristicas)
+                setValues({
+                    descricao: `${res.data.descricao}`,
+                    especie: `${res.data.especie}`,
+                    id: `${res.data.id}`,
+                    idade: `${res.data.idade}`,
+                    instituicao: `${res.data.instituicao}`,
+                    isDoente: `${res.data.isDoente}`,
+                    mimosPorMes: `${res.data.mimosPorMes}`,
+                    nome: `${res.data.nome}`,
+                    porte: `${res.data.porte}`,
+                    raca: `${res.data.raca}`
+                })
+                //console.log(res.data)
+            })
+        }
     }, [])
+
+    function handleChange(event) {
+        const { value, name } = event.target;
+        setValues({ ...values, [name]: value, })
+    }
+
+    function handleSubmitPet(event) {
+        event.preventDefault()
+        let json = {
+            id: infoPet.id,
+            nome: infoPet.nome,
+            especie: infoPet.especie,
+            raca: infoPet.raca,
+            porte: infoPet.porte,
+            sexo: infoPet.sexo,
+            descricao: infoPet.descricao,
+            caminhoImagem: infoPet.caminhoImagem,
+            doente: infoPet.isDoente,
+            adotado: infoPet.adotado,
+            instituicao: infoPet.instituicao,
+            caracteristicas: infoPet.caracteristicas
+        }
+        api.put(`/pets/${infoPet.id}`, json, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((res) => {
+                swal.fire({
+                    icon: "success",
+                    title: <h2>Pet atualizado com sucesso!</h2>,
+                }).then(() => {
+                    navigate(`/perfil-pet-instituicao/${infoPet.id}`)
+                })
+            }).catch((error) => {
+                swal.fire({
+                    icon: "error",
+                    title: <h2>Ops! Algo deu errado da nossa parte :(</h2>,
+                    text: "Por favor, tente novamente!"
+                });
+                console.log(error)
+            })
+        }
 
     return(
         <>
@@ -59,17 +141,17 @@ function PerfilPetInstituicao() {
                                     <div className="perfil-pet-instituicao-info">
                                         <div className="perfil-pet-instituicao-sexo">
                                             <p>Porte: </p>
-                                            <input type="text" value={infoPet.porte}/>
+                                            <input type="text" name="porte" value={values.porte} onChange={handleChange}/>
                                         </div>
                                         <div className="perfil-pet-instituicao-raca">
                                             <p>Raça: </p>
-                                            <input type="text" value={infoPet.raca}/>
+                                            <input type="text" name="raca" value={values.raca} onChange={handleChange}/>
                                         </div>
                                     </div>
                                     <div className="perfil-pet-instituicao-info">
                                         <div className="perfil-pet-instituicao-doente">
                                             <p>Doente: </p>
-                                            <select name="isDoente" id="isDoente" value={infoPet.isDoente}>
+                                            <select name="isDoente" id="isDoente" value={values.isDoente} onChange={handleChange}>
                                                 <option value="true">Sim</option>
                                                 <option value="false">Não</option>
                                             </select>
@@ -81,7 +163,7 @@ function PerfilPetInstituicao() {
                             <div className="perfil-pet-instituicao-descricao-pet-container">
                                 <div className="perfil-pet-instituicao-descricao-pet">
                                     <p>Descrição: </p>
-                                    <textarea id="" cols="67" rows="6" value={infoPet.descricao}></textarea>
+                                    <textarea id="" cols="67" rows="6" name="descricao" value={values.descricao} onChange={handleChange}></textarea>
                                 </div>
                             </div>
                         
@@ -109,7 +191,7 @@ function PerfilPetInstituicao() {
                 </div>
                 <div className="perfil-pet-instituicao-btn">
                     <button>Adicionar Mimos</button>
-                    <button>Editar</button>
+                    <button type="submit" onClick={handleSubmitPet}>Salvar</button>
                 </div>
             </div>
             
