1b9823b2f59f7e2032f0bed0eccad6a2acfb213a
erikpacheeco <erik.pacheco@bandtec.com.br>
2022-10-09 20:30:09 -0300

    construção da página perfil usuario

--- petfinder-app/src/Pages/PerfilUsuario/index.jsx
+++ petfinder-app/src/Pages/PerfilUsuario/index.jsx
@@ -2,6 +2,27 @@ import './perfil-usuario.css';
 
 import HeaderApp from "../../Components/HeaderApp";
 import React from "react";
+import EditarIcon from "../../Images/edit-two.svg"
+import PerfilUsuarioFuncoes from "./perfil-usuario-funcoes.js"
+// import { Chart } from "react-google-charts";
+import MedalhaNoBronze from "./../../Images/pet-friendly-No-bronze.svg"
+import MedalhaNoPrata from "./../../Images/pet-friendly-No-prata.svg"
+import MedalhaNoOuro from "./../../Images/pet-friendly-No-ouro.svg"
+
+// function VerificarComplemento(objUser){
+//     if (objUser.endereco.complemento == null) {
+//         return (
+//             <input className="perfil-usuario-texto perfil-usuario-input"
+//                 id="email"
+//                 type="text" 
+//                 name="email"
+//                 value={objUser.endereco.complemento}
+//                 // onChange={}
+//             /> 
+//         )
+        
+//     }
+// }
 
 function PerfilUsuario(){
     
@@ -17,85 +38,104 @@ function PerfilUsuario(){
                     </div>
                     <div className="perfil-usuario-box">
                         <form>
-                            <div className="perfil-usuario-informacoes-pessoal">
-                                <div className="perfil-usuario-informacoes-container">
-                                    <div className="perfil-usuario-informacoes-edit">
-                                        <input className="perfil-usuario-titulo perfil-usuario-input perfil-usuario-input85"
+                            <div className="perfil-usuario-card">
+                                <div className="perfil-usuario-card-container">
+                                    <div className="perfil-usuario-div-edit">
+                                        <input className="perfil-usuario-titulo perfil-usuario-input"
                                             id="nome"
                                             type="text" 
                                             name="nome"
                                             value={objUser.nome}
                                             // onChange={}
                                         />
-                                        <button>Editar</button>
+                                        <button className="perfil-usuario-btn-edit" onClick={PerfilUsuarioFuncoes}>
+                                            <span>Editar</span>
+                                            <img src={EditarIcon} alt="Editar" />
+                                        </button>
+                                        {/* <button className="perfil-usuario-btn-salvar">
+                                            <span>Salvar</span>
+                                            <img src={EditarIcon} alt="Salvar" />
+                                        </button> */}
                                     </div>
-                                    <input className="perfil-usuario-texto perfil-usuario-input"
-                                        id="email"
-                                        type="text" 
-                                        name="email"
-                                        value={objUser.email}
-                                        // onChange={}
-                                    />
+                                    <div className="perfil-usuario-div-edit">
+                                        <input className="perfil-usuario-texto perfil-usuario-input"
+                                            id="email"
+                                            type="text" 
+                                            name="email"
+                                            value={objUser.email}
+                                            // onChange={}
+                                        />
+                                    </div>
                                 </div>
                             </div>
                             
-                            <div className="perfil-usuario-informacoes-endereco">
-                                <div className="perfil-usuario-informacoes-container">
-                                    <span>Endereço</span>
-                                    <div>
-                                        <input className="perfil-usuario-texto"
-                                            id="cidade"
+                           <div className="perfil-usuario-card">
+                                <div className="perfil-usuario-card-container">
+                                    <span className="perfil-usuario-titulo">Endreço</span>
+                                    <div className="perfil-usuario-div-edit">
+                                        <input className="perfil-usuario-texto perfil-usuario-input"
+                                            id="email"
                                             type="text" 
-                                            name="cidade"
+                                            name="email"
                                             value={objUser.endereco.rua}
                                             // onChange={}
                                         />
-                                        <input className="perfil-usuario-texto"
-                                            id="cidade"
+                                        <input className="perfil-usuario-texto perfil-usuario-input"
+                                            id="email"
                                             type="text" 
-                                            name="cidade"
+                                            name="email"
                                             value={objUser.endereco.num}
                                             // onChange={}
                                         />
-                                        {/* <input className="perfil-usuario-texto"
-                                            id="cidade"
+                                        {/* VerificarComplemento({objUser}); */}
+                                        <input className="perfil-usuario-texto perfil-usuario-input"
+                                            id="email"
                                             type="text" 
-                                            name="cidade"
-                                            value={objUser.endereco.complemento == null ? "" : objUser.endereco.complemento}
-                                            // onChange={}
-                                        /> */}
-                                        <input className="perfil-usuario-texto"
-                                            id="cidade"
-                                            type="text" 
-                                            name="cidade"
-                                            value={objUser.endereco.complemento == null ? "" : objUser.endereco.complemento}
+                                            name="email"
+                                            value={objUser.endereco.bairro}
                                             // onChange={}
                                         />
                                     </div>
-                                    <div>
-                                        <input className="perfil-usuario-texto"
-                                            id="cidade"
+                                    <div className="perfil-usuario-div-edit">
+                                        <input className="perfil-usuario-texto perfil-usuario-input"
+                                            id="email"
                                             type="text" 
-                                            name="cidade"
+                                            name="email"
                                             value={objUser.endereco.cidade}
                                             // onChange={}
                                         />
-                                        <input className="perfil-usuario-texto"
-                                            id="estado"
+                                        <input className="perfil-usuario-texto perfil-usuario-input"
+                                            id="email"
                                             type="text" 
-                                            name="estado"
+                                            name="email"
                                             value={objUser.endereco.uf}
                                             // onChange={}
                                         />
                                     </div>
                                 </div>
-                            </div>
-                            
-                            <div className="perfil-usuario-informacoes-pontuacao">
-                                <div className="perfil-usuario-informacoes-container">
+                           </div>
 
+                            <div className="perfil-usuario-card">
+                                <div className="perfil-usuario-card-container">
+                                    <div className="perfil-usuario-titulo">
+                                        <span>Pontuação</span>
+                                    </div>
+                                    <div className="perfil-usuario-pontuacao">
+                                        <div className="perfil-usuario-pontuacao-dash">
+                                            
+                                        </div>
+                                        <div className="perfil-usuario-pontuacao-info">
+                                            <span>Total: </span>
+                                            <span>Próxima Medalha: </span>
+                                            <div className="perfil-usuario-medalha">
+                                                <img src={MedalhaNoBronze} alt="MedalhaNoBronze" />
+                                                <img src={MedalhaNoPrata} alt="MedalhaNoPrata" />
+                                                <img src={MedalhaNoOuro} alt="MedalhaNoOuro" />
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                           </div>
                         </form>
 
                         <div className="perfil-usuario-pets-apadrinhado">
---
+++ petfinder-app/src/Pages/PerfilUsuario/perfil-usuario-funcoes.js
@@ -0,0 +1,5 @@
+function mudarParaSalvar(){
+    document.getElementByClassName("perfil-usuario-btn-edit")[0].style.display = "none";
+}
+
+export default mudarParaSalvar;
