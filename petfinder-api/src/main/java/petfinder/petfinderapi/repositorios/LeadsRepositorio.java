63839aae36961c5f53a3b2b824136b63bcdef703
RafaAmancioSoares <rafaela.soares@bandtec.com.br>
2022-10-16 03:31:53 -0300

    Adicionando endpoints para os graficos das dashboards

--- petfinder-api/src/main/java/petfinder/petfinderapi/repositorios/LeadsRepositorio.java
+++ petfinder-api/src/main/java/petfinder/petfinderapi/repositorios/LeadsRepositorio.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 public interface LeadsRepositorio extends JpaRepository<Leads, Integer> {
 
-    @Query(value="select data_cadastro from Leads l where l.tipo != 'sysadm' and l.data_cadastro like CONCAT(?1,'-',?2,'%')", nativeQuery=true)
-    public List getLeadPorMes(String ano, String mes);
+    @Query(value="select count(data_cadastro) from Leads l where l.tipo != 'sysadm' and l.data_cadastro like CONCAT(?1,'-',?2,'%')", nativeQuery=true)
+    public int countLeadPorMes(String ano, String mes);
 
-    @Query(value="select data_cadastro from Leads l where l.tipo = 'user' and l.data_cadastro like CONCAT(?1,'-',?2,'%')", nativeQuery=true)
-    public List getLeadUsuarioPorMes(String ano, String mes);
+    @Query(value="select count(data_cadastro) from Leads l where l.tipo = 'user' and l.data_cadastro like CONCAT(?1,'-',?2,'%')", nativeQuery=true)
+    public int countLeadUsuarioPorMes(String ano, String mes);
 
-    @Query(value="select data_cadastro from Leads l where l.tipo = 'adm' or l.tipo = 'chatops' and l.data_cadastro like CONCAT(?1,'-',?2,'%')", nativeQuery=true)
-    public List getLeadInstituicaoPorMes(String ano, String mes);
+    @Query(value="select count(data_cadastro) from Leads l where l.tipo = 'adm' or l.tipo = 'chatops' and l.data_cadastro like CONCAT(?1,'-',?2,'%')", nativeQuery=true)
+    public int countLeadInstituicaoPorMes(String ano, String mes);
 
     @Query(value = "SELECT * FROM leads WHERE data_cadastro BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE() AND tipo != 'user' AND tipo != 'sysadmin'", nativeQuery = true)
     public List getUltimoMês();
