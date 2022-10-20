362c6aaa70fa6793513a050df306d7809a67d281
Ubuntu <enzo.godoy@bandtec.com.br>
2022-10-20 03:11:53 +0000

    Atualizando script de alteração de ambiente react com IP elástico

--- prod-log/fe-deploy/ApiMsg.js
+++ prod-log/fe-deploy/ApiMsg.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import host from "./Host.js";
 
 const api_msg = axios.create({
-    baseURL:`http://3.238.81.12:8081/api-msg`
+    baseURL:`http://54.227.142.24:8081/api-msg`
 }) 
 
 export default api_msg;
