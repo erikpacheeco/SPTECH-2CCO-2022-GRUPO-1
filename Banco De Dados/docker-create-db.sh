39b1c9394e1809a7a6ea684ac7cd112f518258d4
Lucas Mesquita de Souza <lucas.msouza@bandtec.com.br>
2022-10-20 01:29:13 -0300

    update insert collation

--- Banco De Dados/docker-create-db.sh
+++ Banco De Dados/docker-create-db.sh
@@ -1,2 +1,2 @@
-mysql < ./create_db.sql -u root -purubu100
-mysql < ./insert_dados.sql -u root -purubu100
+mysql < ./create_db.sql -u root -purubu100 --default-character-set=utf8
+mysql < ./insert_dados.sql -u root -purubu100 --default-character-set=utf8
