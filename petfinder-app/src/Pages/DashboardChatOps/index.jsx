a34c2eecae3373e4a9848a1fd77f52418d56406b
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-11-22 23:31:24 -0300

    remove old code dashboards frontend

--- petfinder-app/src/Pages/DashboardChatOps/index.jsx
+++ petfinder-app/src/Pages/DashboardChatOps/index.jsx
@@ -14,134 +14,7 @@ function DashboardChatOps() {
 
     const infoUsuario = JSON.parse(localStorage.getItem('petfinder_user'));
 
-    const [infoDashboard, setInfoDashboard] = useState([]);
-
-    const [infoDemandaComparaInst, setInfoDemandaComparaInst] = useState([]);
-    const [infoDemandaComparaUsuario, setInfoDemandaComparaUsuario] = useState([]);
-    const [infoDemandaComparaCancelada, setInfoDemandaComparaCancelada] = useState([]);
-
-    // const [infoDemandaSemana1, setInfoDemandaSemana1] = useState([]);
-    // const [infoDemandaSemana2, setInfoDemandaSemana2] = useState([]);
-    // const [infoDemandaSemana3, setInfoDemandaSemana3] = useState([]);
-    // const [infoDemandaSemana4, setInfoDemandaSemana4] = useState([]);
-    // const [infoDemandaSemana5, setInfoDemandaSemana5] = useState([]);
-    // const [infoDemandaSemana6, setInfoDemandaSemana6] = useState([]);
-    // const [infoDemandaSemana7, setInfoDemandaSemana7] = useState([]);
-
-    // const [infoDemandaMes1, setInfoDemandaMes1] = useState([]);
-    // const [infoDemandaMes2, setInfoDemandaMes2] = useState([]);
-    // const [infoDemandaMes3, setInfoDemandaMes3] = useState([]);
-    // const [infoDemandaMes4, setInfoDemandaMes4] = useState([]);
-    // const [infoDemandaMes5, setInfoDemandaMes5] = useState([]);
-    // const [infoDemandaMes6, setInfoDemandaMes6] = useState([]);
-
-    const [infoDemandaSemanaAdocao, setInfoDemandaSemanaAdocao] = useState([]);
-    const [infoDemandaSemanaPagamento, setInfoDemandaSemanaPagamento] = useState([]);
-    const [infoDemandaMesAdocao, setInfoDemandaMesAdocao] = useState([]);
-    const [infoDemandaMesPagamento, setInfoDemandaMesPagamento] = useState([]);
-
-    const [infoDia1, setInfoDia1] = useState([]);
-    const [infoDia2, setInfoDia2] = useState([]);
-    const [infoDia3, setInfoDia3] = useState([]);
-    const [infoDia4, setInfoDia4] = useState([]);
-    const [infoDia5, setInfoDia5] = useState([]);
-    const [infoDia6, setInfoDia6] = useState([]);
-    const [infoDia7, setInfoDia7] = useState([]);
-
-    const [infoMes1, setInfoMes1] = useState([]);
-    const [infoMes2, setInfoMes2] = useState([]);
-    const [infoMes3, setInfoMes3] = useState([]);
-    const [infoMes4, setInfoMes4] = useState([]);
-    const [infoMes5, setInfoMes5] = useState([]);
-    const [infoMes6, setInfoMes6] = useState([]);
-
     var trocaBtnDemanda = true;
-
-    useEffect(() => {
-
-        const interval = setInterval(() => {
-            document.querySelector("#btn-semana-demanda").click();
-            return clearInterval(interval);
-        }, 400);
-
-    }, []);
-
-    // Cards
-    useEffect(() => {
-    
-        api.get(`/demandas/dashboard/${infoUsuario.id}`).then((res) => {
-            setInfoDashboard(res.data)
-        })
-
-        const dataAtual = new Date();
-        const mesAtual = dataAtual.getMonth() + 1;
-        const diaAtual = dataAtual.getDate();
-
-        setInfoDia7(diaAtual)
-        setInfoDia6(diaAtual-1)
-        setInfoDia5(diaAtual-2)
-        setInfoDia4(diaAtual-3)
-        setInfoDia3(diaAtual-4)
-        setInfoDia2(diaAtual-5)
-        setInfoDia1(diaAtual-6)
-
-        setInfoMes6(mesAtual)
-        setInfoMes5(mesAtual-1)
-        setInfoMes4(mesAtual-2)
-        setInfoMes3(mesAtual-3)
-        setInfoMes2(mesAtual-4)
-        setInfoMes1(mesAtual-5)
-        
-    }, [])
-
-    // grafico demandas 
-    useEffect(() => {
-
-        const dataAtual = new Date();
-        const mesAtual = dataAtual.getMonth() + 1;
-
-        api.get(`/usuarios/demanda/${infoUsuario.fkInstituicao.id}/${infoUsuario.id}/${dataAtual.getFullYear()}/0${mesAtual-1}`).then((res) => {
-            setInfoDemandaComparaInst(res.data[0])
-            setInfoDemandaComparaUsuario(res.data[1])
-            setInfoDemandaComparaCancelada(res.data[2])
-        })
-
-    }, [])
-
-    // grafico demandas 
-    useEffect(() => {
-
-        api.get(`/demandas/dashboard/usuario/${infoUsuario.fkInstituicao.id}/${infoUsuario.id}`).then((res) => {
-            setInfoDemandaMesAdocao(res.data.demandaAdocaoMes)
-            setInfoDemandaSemanaAdocao(res.data.demandaAdocaoSemana)
-            setInfoDemandaMesPagamento(res.data.demandaPagamentoMes)
-            setInfoDemandaSemanaPagamento(res.data.demandaPagamentoSemana)
-        })
-
-    }, [])
-
-    const dataDemandaMes = [
-        ["Mês", "Adoção", "Pagamento"], 
-        ["0"+infoMes1, infoDemandaMesAdocao[0], infoDemandaMesPagamento[0]], 
-        ["0"+infoMes2, infoDemandaMesAdocao[1], infoDemandaMesPagamento[1]], 
-        ["0"+infoMes3, infoDemandaMesAdocao[2], infoDemandaMesPagamento[2]], 
-        ["0"+infoMes4, infoDemandaMesAdocao[3], infoDemandaMesPagamento[3]], 
-        ["0"+infoMes5, infoDemandaMesAdocao[4], infoDemandaMesPagamento[4]], 
-        ["0"+infoMes6, infoDemandaMesAdocao[5], infoDemandaMesPagamento[5]]
-    ]
-
-    const dataDemandaSemana = [
-        ["Dia", "Adoção", "Pagamento"], 
-        [infoDia1, infoDemandaSemanaAdocao[0], infoDemandaSemanaPagamento[0]], 
-        [infoDia2, infoDemandaSemanaAdocao[1], infoDemandaSemanaPagamento[1]], 
-        [infoDia3, infoDemandaSemanaAdocao[2], infoDemandaSemanaPagamento[2]], 
-        [infoDia4, infoDemandaSemanaAdocao[3], infoDemandaSemanaPagamento[3]], 
-        [infoDia5, infoDemandaSemanaAdocao[4], infoDemandaSemanaPagamento[4]], 
-        [infoDia6, infoDemandaSemanaAdocao[5], infoDemandaSemanaPagamento[5]],
-        [infoDia7, infoDemandaSemanaAdocao[6], infoDemandaSemanaPagamento[6]]
-    ]
-
-    const [valorDataDemanda, setValorDataDemanda] = useState(dataDemandaSemana);
 
     return(
         <>
@@ -153,12 +26,12 @@ function DashboardChatOps() {
                         <h2>Métricas de cadastro</h2>
                         <div className="dashboard-chatops-metricas-card-container">
                             <div className="dashboard-chatops-metricas-card">
-                                <p>{infoDashboard.qtdDemandaAbertaInstituicao}</p>
+                                <p>{15}</p>
                                 <p>Em espera</p>
                             </div>
 
                             <div className="dashboard-chatops-metricas-card">
-                                <p>{infoDashboard.qtdDemandaConcluidaColaboradorInstiuicao}</p>
+                                <p>{20}</p>
                                 <p>Concluídas</p>
                             </div>
 
@@ -175,9 +48,9 @@ function DashboardChatOps() {
                                         chartType="PieChart"
                                         data={[
                                             ["", "Demanda"], 
-                                            ["Sua equipe (com sucesso)", infoDemandaComparaInst - infoDemandaComparaUsuario], 
-                                            ["Você (com sucesso)", infoDemandaComparaUsuario], 
-                                            ["Sem sucesso", infoDemandaComparaCancelada]
+                                            ["Sua equipe (com sucesso)", 2], 
+                                            ["Você (com sucesso)", 10], 
+                                            ["Sem sucesso", 3]
                                         ]}
                                         width="100%"
                                         height="100%"
@@ -196,18 +69,7 @@ function DashboardChatOps() {
                                         type="button"
                                         className="btn-semana-demanda"
                                         id='btn-semana-demanda'
-                                        onClick={() => {
-                                            if(trocaBtnDemanda){
-                                                let btnSemanaDemanda = document.getElementById("btn-semana-demanda");
-                                                btnSemanaDemanda.style.backgroundColor = "#7F2AB5";
-                                                btnSemanaDemanda.style.color = "white";
-
-                                                let btnMesDemanda = document.getElementById("btn-mes-demanda");
-                                                btnMesDemanda.style.backgroundColor = "white";
-                                                btnMesDemanda.style.color = "#7F2AB5";
-                                            }
-                                            setValorDataDemanda(dataDemandaSemana);
-                                        }}
+                                        onClick={() => {}}
                                     >
                                         Semana
                                     </button>
@@ -216,18 +78,7 @@ function DashboardChatOps() {
                                         type="button"
                                         className="btn-mes-demanda"
                                         id='btn-mes-demanda'
-                                        onClick={() => {
-                                            if(trocaBtnDemanda){
-                                                let btnSemanaDemanda = document.getElementById("btn-semana-demanda");
-                                                btnSemanaDemanda.style.backgroundColor = "white";
-                                                btnSemanaDemanda.style.color = "#7F2AB5";
-
-                                                let btnMesDemanda = document.getElementById("btn-mes-demanda");
-                                                btnMesDemanda.style.backgroundColor = "#7F2AB5";
-                                                btnMesDemanda.style.color = "white";
-                                            }
-                                            setValorDataDemanda(dataDemandaMes);
-                                        }}
+                                        onClick={() => {}}
                                     >
                                         Mês
                                     </button>
@@ -236,7 +87,14 @@ function DashboardChatOps() {
                                 <div className="dashboard-chatops-metricas-grafico-container">
                                     <Chart
                                         chartType="Bar"
-                                        data={valorDataDemanda}
+                                        data={[
+                                            ["Dia", "Pagamento", "Adoção"],
+                                            ["11/18", 20, 18],
+                                            ["11/19", 10, 12],
+                                            ["11/20", 15, 14],
+                                            ["11/21", 14, 12],
+                                            ["11/22", 17, 20],
+                                        ]}
                                         width="100%"
                                         height="100%"
                                         legendToggle
--- petfinder-app/src/Pages/DashboardSysAdmin/index.jsx
+++ petfinder-app/src/Pages/DashboardSysAdmin/index.jsx
@@ -12,152 +12,6 @@ export const options = {
 
 function DashboardSysAdmin() {
 
-    const infoUsuario = JSON.parse(localStorage.getItem('petfinder_user'));
-
-    const [infoDashboard, setInfoDashboard] = useState([]);
-
-    const [infoVisitanteMes1, setInfoVisitanteMes1] = useState(0);
-    const [infoVisitanteMes2, setInfoVisitanteMes2] = useState(0);
-    const [infoVisitanteMes3, setInfoVisitanteMes3] = useState(0);
-    const [infoVisitanteMes4, setInfoVisitanteMes4] = useState(0);
-    const [infoVisitanteMes5, setInfoVisitanteMes5] = useState(0);
-    const [infoVisitanteMes6, setInfoVisitanteMes6] = useState(0);
-
-    const [infoLeadUsuarioMes1, setInfoLeadUsuarioMes1] = useState(0);
-    const [infoLeadUsuarioMes2, setInfoLeadUsuarioMes2] = useState(0);
-    const [infoLeadUsuarioMes3, setInfoLeadUsuarioMes3] = useState(0);
-    const [infoLeadUsuarioMes4, setInfoLeadUsuarioMes4] = useState(0);
-    const [infoLeadUsuarioMes5, setInfoLeadUsuarioMes5] = useState(0);
-    const [infoLeadUsuarioMes6, setInfoLeadUsuarioMes6] = useState(0);
-
-    const [infoLeadMes1, setInfoLeadMes1] = useState(0);
-    const [infoLeadMes2, setInfoLeadMes2] = useState(0);
-    const [infoLeadMes3, setInfoLeadMes3] = useState(0);
-    const [infoLeadMes4, setInfoLeadMes4] = useState(0);
-    const [infoLeadMes5, setInfoLeadMes5] = useState(0);
-    const [infoLeadMes6, setInfoLeadMes6] = useState(0);
-
-    const [infoTotalLeadInstituicao, setInfoTotalLeadInstituicao] = useState(0);
-    const [infoLeadInstituicaoAtivo, setInfoLeadInstituicaoAtivo] = useState(0);
-
-    const [infoMes1, setInfoMes1] = useState(0);
-    const [infoMes2, setInfoMes2] = useState(0);
-    const [infoMes3, setInfoMes3] = useState(0);
-    const [infoMes4, setInfoMes4] = useState(0);
-    const [infoMes5, setInfoMes5] = useState(0);
-    const [infoMes6, setInfoMes6] = useState(0);
-
-    // Cards
-    useEffect(() => {
-
-        api.get(`/demandas/dashboard/${infoUsuario.id}`).then((res) => {
-            setInfoDashboard(res.data)
-        })
-
-    }, [])
-
-    // Gráfico visitante
-    useEffect(() => {
-
-        const dataAtual = new Date();
-        const mesAtual = dataAtual.getMonth() + 1;
-
-        api.get(`/usuarios/grafico-visitante/${dataAtual.getFullYear()}/0${mesAtual - 1}`).then((res) => {
-            setInfoVisitanteMes6(res.data[0])
-            setInfoLeadMes6(res.data[1])
-            setInfoMes6(mesAtual - 1)
-        }).then(() => {
-            api.get(`/usuarios/grafico-visitante/${dataAtual.getFullYear()}/0${mesAtual - 2}`).then((res) => {
-                setInfoVisitanteMes5(res.data[0])
-                setInfoLeadMes5(res.data[1])
-                setInfoMes5(mesAtual - 2)
-            })
-        }).then(() => {
-            api.get(`/usuarios/grafico-visitante/${dataAtual.getFullYear()}/0${mesAtual - 3}`).then((res) => {
-                setInfoVisitanteMes4(res.data[0])
-                setInfoLeadMes4(res.data[1])
-                setInfoMes4(mesAtual - 3)
-            })
-        }).then(() => {
-            api.get(`/usuarios/grafico-visitante/${dataAtual.getFullYear()}/0${mesAtual - 4}`).then((res) => {
-                setInfoVisitanteMes3(res.data[0])
-                setInfoLeadMes3(res.data[1])
-                setInfoMes3(mesAtual - 4)
-            })
-        }).then(() => {
-            api.get(`/usuarios/grafico-visitante/${dataAtual.getFullYear()}/0${mesAtual - 5}`).then((res) => {
-                setInfoVisitanteMes2(res.data[0])
-                setInfoLeadMes2(res.data[1])
-                setInfoMes2(mesAtual - 5)
-            })
-        }).then(() => {
-            api.get(`/usuarios/grafico-visitante/${dataAtual.getFullYear()}/0${mesAtual - 6}`).then((res) => {
-                setInfoVisitanteMes1(res.data[0])
-                setInfoLeadMes1(res.data[1])
-                setInfoMes1(mesAtual - 6)
-            })
-        })
-    }, [])
-
-    // Gráfico lead Usuário
-    useEffect(() => {
-
-        const dataAtual = new Date();
-        const mesAtual = dataAtual.getMonth() + 1;
-
-        api.get(`/usuarios/lead-usuario/${dataAtual.getFullYear()}/0${mesAtual - 1}`).then((res) => {
-            setInfoLeadUsuarioMes6(res.data)
-            setInfoMes6(mesAtual - 1)
-        }).then(() => {
-            api.get(`/usuarios/lead-usuario/${dataAtual.getFullYear()}/0${mesAtual - 2}`).then((res) => {
-                setInfoLeadUsuarioMes5(res.data)
-                setInfoMes5(mesAtual - 2)
-            })
-        }).then(() => {
-            api.get(`/usuarios/lead-usuario/${dataAtual.getFullYear()}/0${mesAtual - 3}`).then((res) => {
-                setInfoLeadUsuarioMes4(res.data)
-                setInfoMes4(mesAtual - 3)
-            })
-        }).then(() => {
-            api.get(`/usuarios/lead-usuario/${dataAtual.getFullYear()}/0${mesAtual - 4}`).then((res) => {
-                setInfoLeadUsuarioMes3(res.data)
-                setInfoMes3(mesAtual - 4)
-            })
-        }).then(() => {
-            api.get(`/usuarios/lead-usuario/${dataAtual.getFullYear()}/0${mesAtual - 5}`).then((res) => {
-                setInfoLeadUsuarioMes2(res.data)
-                setInfoMes2(mesAtual - 5)
-            })
-        }).then(() => {
-            api.get(`/usuarios/lead-usuario/${dataAtual.getFullYear()}/0${mesAtual - 6}`).then((res) => {
-                setInfoLeadUsuarioMes1(res.data)
-                setInfoMes1(mesAtual - 6)
-            })
-        })
-
-    }, [])
-
-    // Gráfico Lead Instituicao
-    useEffect(() => {
-
-        let soma = 0;
-        let lead = 0;
-
-        api.get(`/usuarios/lead-instituicao`).then((res) => {
-
-            res.data.forEach(element => {
-                soma += element;
-            });
-
-            setInfoTotalLeadInstituicao(soma);
-            lead = res.data[0]            
-        })
-        .then(() => {
-            setInfoLeadInstituicaoAtivo(lead - infoTotalLeadInstituicao)
-        })
-
-    }, [])
-
     return (
         <>
             <HeaderApp />
@@ -168,27 +22,27 @@ function DashboardSysAdmin() {
                         <h2>Métricas de cadastro</h2>
                         <div className="dashboard-sysadmin-metricas-card-container">
                             <div className="dashboard-sysadmin-metricas-card">
-                                <p>{infoDashboard.qtdInstituicao}</p>
+                                <p>{10}</p>
                                 <p>Instituições</p>
                             </div>
 
                             <div className="dashboard-sysadmin-metricas-card">
-                                <p>{infoDashboard.qtdUsuario}</p>
+                                <p>{20}</p>
                                 <p>Usuários</p>
                             </div>
 
                             <div className="dashboard-sysadmin-metricas-card">
-                                <p>{infoDashboard.qtdAnimal}</p>
+                                <p>{21}</p>
                                 <p>Animais</p>
                             </div>
 
                             <div className="dashboard-sysadmin-metricas-card">
-                                <p>{infoDashboard.qtdPadrinho}</p>
+                                <p>{24}</p>
                                 <p>Padrinhos</p>
                             </div>
 
                             <div className="dashboard-sysadmin-metricas-card">
-                                <p>{infoDashboard.qtdAdmin}</p>
+                                <p>{44}</p>
                                 <p>Administradores</p>
                             </div>
                         </div>
@@ -204,12 +58,12 @@ function DashboardSysAdmin() {
                                         chartType="Bar"
                                         data={[
                                             ["Mês", "Qtd Visitantes", "Qtd Usuários"],
-                                            ["0" + infoMes1, infoVisitanteMes1, infoLeadMes1],
-                                            ["0" + infoMes2, infoVisitanteMes2, infoLeadMes2],
-                                            ["0" + infoMes3, infoVisitanteMes3, infoLeadMes3],
-                                            ["0" + infoMes4, infoVisitanteMes4, infoLeadMes4],
-                                            ["0" + infoMes5, infoVisitanteMes5, infoLeadMes5],
-                                            ["0" + infoMes6, infoVisitanteMes6, infoLeadMes6]
+                                            ["2022/06", 10, 15],
+                                            ["2022/07", 12, 12],
+                                            ["2022/08", 15, 16],
+                                            ["2022/09", 11, 11],
+                                            ["2022/10", 14, 15],
+                                            ["2022/11", 12, 14]
                                         ]}
                                         width="100%"
                                         height="100%"
@@ -225,12 +79,12 @@ function DashboardSysAdmin() {
                                         chartType="Bar"
                                         data={[
                                             ["Mês", "Qtd Leads", "Qtd Clientes"],
-                                            ["0" + infoMes1, infoLeadUsuarioMes1[0], infoLeadUsuarioMes1[1]],
-                                            ["0" + infoMes2, infoLeadUsuarioMes2[0], infoLeadUsuarioMes2[1]],
-                                            ["0" + infoMes3, infoLeadUsuarioMes3[0], infoLeadUsuarioMes3[1]],
-                                            ["0" + infoMes4, infoLeadUsuarioMes4[0], infoLeadUsuarioMes4[1]],
-                                            ["0" + infoMes5, infoLeadUsuarioMes5[0], infoLeadUsuarioMes5[1]],
-                                            ["0" + infoMes6, infoLeadUsuarioMes6[0], infoLeadUsuarioMes6[1]]
+                                            ["2022/06", 10, 15],
+                                            ["2022/07", 12, 14],
+                                            ["2022/08", 15, 11],
+                                            ["2022/09", 11, 16],
+                                            ["2022/10", 12, 15],
+                                            ["2022/11", 13, 12]
                                         ]}
                                         width="100%"
                                         height="100%"
@@ -250,7 +104,7 @@ function DashboardSysAdmin() {
                                         data={[
                                             ["Ativas", "Inativas"],
                                             ["Ativas", 4],
-                                            ["Inativas", 0],
+                                            ["Inativas", 1],
                                         ]}
                                         options={{
                                             title: "",
