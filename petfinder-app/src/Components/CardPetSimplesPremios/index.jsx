6ec8b94fcebe4eee3d85d42089a557ad4ae885e0
RafaAmancioSoares <rafaela.soares@bandtec.com.br>
2022-11-27 01:02:05 -0300

    Adicionando loading de imagem nos meus premios

--- petfinder-app/src/Components/CardPetSimplesPremios/index.jsx
+++ petfinder-app/src/Components/CardPetSimplesPremios/index.jsx
@@ -1,10 +1,17 @@
 import "./card-pet-simples-premios.css"
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function CardPetSimples(props) {
 
     return (
-       <img className="cardPetSimples"src={props.srcImg} alt="" />
+        <LazyLoadImage 
+            effect="blur"
+            className="cardPetSimples"
+            src={props.srcImg} 
+            alt="" 
+        />
     )
 }
 
