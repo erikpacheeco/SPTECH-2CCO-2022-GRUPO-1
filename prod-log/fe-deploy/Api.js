be4f0cfaf65dd8e69691241acb4f59df9c46efe9
Lucas Mesquita de Souza <lucas.msouza@bandtec.com.br>
2022-11-28 17:12:22 -0300

    adding elastic ip

--- prod-log/fe-deploy/Api.js
+++ prod-log/fe-deploy/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import host from "./Host.js";
 
 const api_msg = axios.create({
-    baseURL:`http://54.227.142.24:8080`
+    baseURL:`http://52.203.70.101:8080`
 }) 
 
 export default api_msg;
--- prod-log/fe-deploy/ApiMsg.js
+++ prod-log/fe-deploy/ApiMsg.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import host from "./Host.js";
 
 const api_msg = axios.create({
-    baseURL:`http://54.227.142.24:8081/api-msg`
+    baseURL:`http://52.203.70.101:8081/api-msg`
 }) 
 
 export default api_msg;
