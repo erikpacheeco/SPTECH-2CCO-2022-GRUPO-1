3ee92088ae3da5e7fcf20d4ee71944514cf0760c
RafaAmancioSoares <rafaela.soares@bandtec.com.br>
2022-11-28 10:47:12 -0300

    Adicionando loading de carregamento nos cards de pet

--- petfinder-app/src/Components/CardPet/index.jsx
+++ petfinder-app/src/Components/CardPet/index.jsx
@@ -1,8 +1,18 @@
 import doente from "../../Images/png_img/saude_2.png"
 import "./card-pet.css"
 import React from "react";
+import { useEffect, useState } from "react";
 
 export default function CardPet(props) {
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+        setLoading(false);
+        }, 2000);
+    }, []);
 
     CardPet.defaultProps= {
         isDoente: false,
@@ -10,15 +20,28 @@ export default function CardPet(props) {
     }
 
     return (
-        <figure className="card-pet-img-pet" onClick={props.onClick} style={{backgroundImage: `url("${props.backgroundImage}")`}}>
-            <p style={{'textTransform': "Capitalize"}}>{props.nome}</p>
-            <img
-                className={
-                    props.isDoente ? ("card-pet-info") : ("card-pet-hide")
-                } src={doente}
-                title="Este animal possui alguma doença atualmente "
-                alt=""
-            />
-        </figure>
+        <>
+            {loading ? (
+                <div className="card-loader-container">
+                    <div className="card-spinner"></div>
+                </div>
+                
+            ) : (
+                <figure className="card-pet-img-pet" onClick={props.onClick} 
+                style={{backgroundImage: `url("${props.backgroundImage}")`}}
+                >
+
+                    <p style={{'textTransform': "Capitalize"}}>{props.nome}</p>
+                        <img
+                            className={
+                                props.isDoente ? ("card-pet-info") : ("card-pet-hide")
+                            } 
+                            src={doente}
+                            title="Este animal possui alguma doença atualmente "
+                            alt=""
+                        />
+                </figure>
+            )}
+        </>
     );
 }
